feb241d48520e5c43b7c9d0ba7d042c145f00548
alihamzamohammed <alihamzamohammed2@gmail.com>
2019-11-06 17:18:09 +0000

    rednerer.py ready to execute to main.py

--- renderer.py
+++ renderer.py
@@ -3,15 +3,16 @@ from cocos.text import Label
 from cocos import scene
 from cocos.layer import Layer
 from cocos.director import director
+import main
 
 
-class rendererWindow(Layer):
+class BaseWindow(Layer):
 
     def __init__(self):
-        super(rendererWindow, self).__init__()
+        super(BaseWindow, self).__init__()
 
-director.init(width=1280, height=720, caption="Game", fullscreen=False)
-director.run(scene.Scene(rendererWindow()))
 
-#if __name__=="__main__":
- #   print("This file cannot be run directly, please run main.py to start the game.")
+
+if __name__=="__main__":
+    print("This file cannot be run directly, please run main.py to start the game.")
+    main.main()
