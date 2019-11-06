9d038b9fd33675fb17ed26780e796b5f0b9f37b3
alihamzamohammed <alihamzamohammed2@gmail.com>
2019-11-06 17:00:39 +0000

    disabel fullscreen

--- renderer.py
+++ renderer.py
@@ -10,5 +10,5 @@ class rendererWindow(Layer):
     def __init__(self):
         super(rendererWindow, self).__init__()
 
-director.init(width=1280, height=720, caption="Game", fullscreen=True)
+director.init(width=1280, height=720, caption="Game", fullscreen=False)
 director.run(scene.Scene(rendererWindow()))
