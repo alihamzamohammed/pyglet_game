1a51a5c1d819230ec6e400be3c280a0b0d618493
alihamzamohammed <alihamzamohammed2@gmail.com>
2019-11-08 11:44:40 +0000

    Added configread to read config file into dict

--- main.py
+++ main.py
@@ -3,14 +3,28 @@ import cocos
 from cocos.director import director
 from cocos import scene
 import renderer
+import configparser
 
+defaultconfigfile = "settings.ini"
+config = {}
+
+def configread(configFile):
+    dict1 = {}
+    conf = configparser.ConfigParser.read(configFile)
+    for section in conf.sections():
+        for option in section:
+            dict[option] = conf.get(section, option)
+        config[section] = dict1
+        dict1 = {}
 
 def resourceLoad():
-    pass
+    configread(defaultconfigfile).get()
 
 def main():
     director.init(width=1280, height=720, caption="Game", fullscreen=False)
     director.run(scene.Scene(renderer.loadingScreen()))
-    
+
 if __name__=="__main__":
+    configread()
+    print(config)
     main()  
