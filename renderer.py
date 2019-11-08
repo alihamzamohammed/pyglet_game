7aa20ef375149dbfb73424bcc120665721149941
alihamzamohammed <alihamzamohammed2@gmail.com>
2019-11-08 11:34:58 +0000

    Added loadingScreen()

--- renderer.py
+++ renderer.py
@@ -10,6 +10,10 @@ class BaseWindow(Layer):
     def __init__(self):
         super(BaseWindow, self).__init__()
 
+class loadingScreen(BaseWindow):
+    def __init__(self):
+        super(loadingScreen, self).__init__()
+
 
 if __name__=="__main__":
     print("This file cannot be run directly, please run main.py to start the game.")
