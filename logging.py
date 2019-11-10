04f89994b21df1ee546226bce73f5c6bbd07dd09
alihamzamohammed <alihamzamohammed2@gmail.com>
2019-11-10 22:13:04 +0000

    Added logging system

---
+++ logging.py
@@ -0,0 +1,19 @@
+import main
+import os
+
+logpath = ""
+info = 2
+warning = 1
+error = 0
+
+def log(message, level = info):
+    if level == error:
+        message = "ERROR: " + message
+        print(message)
+        if not logpath == "":
+            log = open(logpath, "a+")
+            log.write(message)
+            log.close()
+
+if main.config["Core"]["Log"] == True:
+    logpath = main.config["Core"]["LogLocation"]
