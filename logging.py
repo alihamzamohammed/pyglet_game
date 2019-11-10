c2db7bcdf82ee3a1408c816e929011196a01e4f2
alihamzamohammed <alihamzamohammed2@gmail.com>
2019-11-10 22:24:08 +0000

    loglevel dict

--- logging.py
+++ logging.py
@@ -2,15 +2,17 @@ import main
 import os
 
 logpath = ""
-info = 2
-warning = 1
-error = 0
+loglevel = {"error": 0, "warning": 1, "info": 2}
 
-def addLog(message, level = info):
-    if level == error:
+def addLog(message, level = loglevel["info"]):
+    if level == loglevel["error"]:
         message = "ERROR: " + message
-        print(message)
-        if not logpath == "":
+    elif level == loglevel["warning"]:
+        message = "WARNING: " + message
+    elif level == loglevel["info"]:
+        message = "INFO: " + message
+    print(message)
+    if not logpath == "":
             log = open(logpath, "a+")
             log.write(message)
             log.close()
