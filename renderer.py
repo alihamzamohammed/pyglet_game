abc50c26a91c258ba9f3e0bc7577f1f9ccbda2ee
alihamzamohammed <alihamzamohammed2@gmail.com>
2019-11-15 12:34:22 +0000

    added title fade in

--- renderer.py
+++ renderer.py
@@ -2,17 +2,10 @@ import cocos
 from cocos.text import Label
 from cocos import scene
 from cocos import layer
+from cocos.actions import *
 from cocos.director import director
 import resources
-
-class title(layer.Layer):
-    title = cocos.text.Label(
-            "Game Title",
-            font_name=resources.font[1],
-            font_size=32,
-            anchor_y="top",
-            anchor_x="center"
-        )
+import time
 
 class BaseWindow(scene.Scene):
 
@@ -20,12 +13,20 @@ class BaseWindow(scene.Scene):
         super(BaseWindow, self).__init__()
 
 class loadingScreen(BaseWindow):
+
     def __init__(self):
         super(loadingScreen, self).__init__()
-        loadingTitle = title.title
+        loadingTitle = cocos.text.Label(
+            "Game Title",
+            font_name=resources.font[1],
+            font_size=32,
+            anchor_y="top",
+            anchor_x="center"
+        )
         x, y = cocos.director.director.get_window_size()
         loadingTitle.position = x / 2, y * 0.95
         self.add(loadingTitle)
+        loadingTitle.do(FadeIn(1))
 
 if __name__=="__main__":
     print("This file cannot be run directly, please run main.py to start the game.")
