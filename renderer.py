56c2b107157280ba4ce80772d5c9843c9bcffb55
alihamzamohammed <alihamzamohammed2@gmail.com>
2020-01-07 11:47:01 +0000

    relegated loading to another thread

--- renderer.py
+++ renderer.py
@@ -7,6 +7,7 @@ from cocos.actions import *
 from cocos.director import director
 import events
 import pyglet
+import threading
 from pyglet import event
 import resources
 
@@ -24,6 +25,22 @@ class BaseWindow(scene.Scene):
 
     def __init__(self):
         super(BaseWindow, self).__init__()
+
+def game_loading():
+    print("Game loading start")
+    logger.addLog("Init levels", logger.loglevel["info"])
+    print(cocos.director.director.scene)
+    # check to see if all levels are in level db, otherwise raise warning, and do not load
+    # Render level thumbnails, and put them as sprite objects into array
+    logger.addLog("Init game modes", logger.loglevel["info"])
+    # check to see if all game modes exist in game mode db
+    # render game mode metadata and add to multidimensional array dict
+    logger.addLog("Init resources", logger.loglevel["info"])
+    # complete
+    logger.addLog("Init items", logger.loglevel["info"])
+    # check to see if all item packs are in item pack db, and if all item xml is without error
+    # add item packs and individual items to multidimensional array
+    events.rendererevents.onProgressFinished()
 
 class MainMenu(BaseWindow):
 
@@ -49,21 +66,12 @@ class loadingScreen(BaseWindow):
     def __init__(self):
         super(loadingScreen, self).__init__()
         x, y = cocos.director.director.get_window_size()
-        #evts = events.RendererEvents()
         title.position = x / 2, y * 0.72
         self.add(title)
-        title.do(FadeIn(1))      
-        logger.addLog("Init levels", logger.loglevel["info"])
-        # check to see if all levels are in level db, otherwise raise warning, and do not load
-        # Render level thumbnails, and put them as sprite objects into array
-        logger.addLog("Init game modes", logger.loglevel["info"])
-        # check to see if all game modes exist in game mode db
-        # render game mode metadata and add to multidimensional array dict
-        logger.addLog("Init resources", logger.loglevel["info"])
-        # complete
-        logger.addLog("Init items", logger.loglevel["info"])
-        # check to see if all item packs are in item pack db, and if all item xml is without error
-        # add item packs and individual items to multidimensional array
+        title.do(FadeIn(1))
+        loadingThread = threading.Thread(target=game_loading)
+        loadingThread.start()
+        print("Thread start called")
         
 def get_scene(scene):
     if scene == "loadingScreen":
