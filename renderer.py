1cf315f8037530330ee85f5f9e3a67810635922e
alihamzamohammed <alihamzamohammed2@gmail.com>
2020-01-16 21:53:28 +0000

    Aligned menulyr and setting button placement

--- renderer.py
+++ renderer.py
@@ -51,7 +51,7 @@ class mainMenu(layer.ColorLayer):
         
         is_event_handler = True
         
-        def __init__(self, label, callbackfunc):
+        def __init__(self, label, callbackfunc, posx, posy):
             super().__init__()
             global x, y
             self.callbackfunc = callbackfunc
@@ -62,12 +62,12 @@ class mainMenu(layer.ColorLayer):
             self.add(bgImage, z=0)
             self.add(lbl, z=1)
             self.anchor = (0, 0)
-            self.x = 0
-            self.y = 0
+            self.x = posx
+            self.y = posy
             self.width = bgImage.width
             self.height = bgImage.height
-            self.width_range = [self.x, self.x + self.width]
-            self.height_range = [self.y, self.y + self.height]
+            self.width_range = [int(self.x), int(self.x + self.width)]
+            self.height_range = [int(self.y), int(self.y + self.height)]
             print(self.width_range)
             print(self.height_range)
         
@@ -81,12 +81,16 @@ class mainMenu(layer.ColorLayer):
     def __init__(self):
         global x, y
         super().__init__(100, 100, 100, 0, width=int(x * 0.4), height=int(y * 0.6))
-        playButton = self.menuItem("Play", self.play)
-        playButton.width_range = [playButton.x + self.x, playButton.x + self.x + playButton.width]
-        playButton.height_range = [playButton.y + self.y, playButton.y + self.y + playButton.height]
+        self.x = (x / 2) - (self.width / 2)
+        self.y = (y * 0.42) - (self.height / 2)
+        print(self.x, self.y)
+        playButton = self.menuItem("Play", self.play, self.x, self.y)
         print(playButton.width_range, playButton.height_range)
         self.add(playButton, z=1)
 
+    def on_enter(self):
+        super().on_enter()
+        self.do(Delay(1) + FadeIn(2))
 
     def play(self):
         pass
@@ -115,11 +119,7 @@ class MainMenuScreen(BaseWindow):
         global x, y
         self.add(titleLabel)
         titleLabel.do(AccelDeccel(MoveTo((x / 2, y * 0.9), 2)))
-        mainmenu = mainMenu()
-        mainmenu.position = (x / 2) - (mainmenu.width / 2), (y * 0.42) - (mainmenu.height / 2)
-        print((x*0.4)-(mainmenu.width/2))
-        self.add(mainmenu)
-        mainmenu.do(Delay(0.5) + FadeIn(0.5))
+        self.add(mainMenu())
 
 
 class loadingScreen(BaseWindow):
