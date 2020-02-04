664023daa0a8283fefac80b499558d00f11c5900
alihamzamohammed <alihamzamohammed2@gmail.com>
2020-02-04 12:38:55 +0000

    Moved title Label and main menu buttons to elements.py

--- elements.py
+++ elements.py
@@ -1,8 +1,83 @@
 import pyglet
 import cocos
 import os
-from cocos.layer import Layer
+from cocos import layer
 from cocos.text import *
 from cocos.actions import *
 from cocos.sprite import *
+
+import resources
+import events
+
+titleLabel = cocos.text.Label(
+    "Game Title",
+    font_name=resources.font[1],
+    font_size=50,
+    anchor_y="top",
+    anchor_x="center"
+)
+
+"""Main Menu Button. Sized and aligned to fit on the main menu, as title buttons. Hardcoded to only use 4, but can be extended and used elsewhere."""
+class MainMenuButton(layer.Layer):
+    
+    is_event_handler = True
+    
+    def __init__(self, label, eventName, buttonorder = 1):
+        super().__init__()
+        global x, y
+        self.eventName = eventName
+        self.label = label
+        self.buttonorder = buttonorder
+        self.bgImage = Sprite("menuButton.png") # Replace with proper resource pack image
+        self.lbl = Label(self.label, anchor_x="center", anchor_y="center")
+        
+        self.x = x / 2
+        self.TOPBTNPOS = 0.68
+        self.y = y * (self.TOPBTNPOS - (0.16 * (buttonorder - 1)))
+                
+        self.width = self.bgImage.width
+        self.height = self.bgImage.height
+        
+        self.add(self.bgImage, z = 0)
+        self.add(self.lbl, z = 1)
+        
+        self.width_range = [int(self.x - (self.bgImage.width / 2)), int(self.x + (self.bgImage.width / 2))]
+        self.height_range = [int(self.y - (self.bgImage.height / 2)), int(self.y + (self.bgImage.height / 2))]
+        
+        self.animate()
+
+        self.schedule_interval(self.setWH, 1)
+        self.resume_scheduler()
+
+    def setWH(self, dt):
+        x, y = director.window.width, director.window.height
+        scalex = x / reswidth
+        scaley = y / resheight
+        self.width_range = [int((x / 2) - ((self.bgImage.width * scalex) / 2)), int((x / 2) + ((self.bgImage.width * scalex) / 2))]
+        self.height_range = [int((y * (self.TOPBTNPOS - (0.16 * (self.buttonorder - 1)))) - ((self.bgImage.height * scaley) / 2)), int((y * (self.TOPBTNPOS - (0.16 * (self.buttonorder - 1)))) + ((self.bgImage.height * scaley) / 2))]
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        if x in range(self.width_range[0], self.width_range[1]) and y in range(self.height_range[0], self.height_range[1]):
+            self.bgImage.image = pyglet.resource.image("menuButtonHovered.png")
+        else:
+            self.bgImage.image = pyglet.resource.image("menuButton.png")
+
+    def on_mouse_press(self, x, y, buttons, modifiers):
+        if x in range(self.width_range[0], self.width_range[1]) and y in range(self.height_range[0], self.height_range[1]):
+            self.eventName()
+            self.bgImage.image = pyglet.resource.image("menuButtonClicked.png")
+            self.lbl.element.color = (0, 0, 0, 255)
+
+    def animate(self):
+        mvx = self.x
+        mvy = self.y
+        # ODD
+        if not self.buttonorder % 2 == 0:
+            self.x = -self.width
+            self.do(Delay(0.7) + AccelDeccel(MoveTo((mvx, mvy), 1.5)))
+        # EVEN
+        else:
+            self.x = x + self.width
+            self.do(Delay(0.7) + AccelDeccel(MoveTo((mvx, mvy), 1.5)))
+
 
--- mainmenu.py
+++ mainmenu.py
@@ -14,6 +14,7 @@ import pyglet
 import pyglet.gl
 import threading
 from pyglet import event
+import elements
 import resources
 import random
 import time
@@ -40,75 +41,8 @@ def game_loading():
 
 
 '''Elements of windows'''
-titleLabel = cocos.text.Label(
-    "Game Title",
-    font_name=resources.font[1],
-    font_size=50,
-    anchor_y="top",
-    anchor_x="center"
-)
-
-class menuItem(layer.Layer):
-    
-    is_event_handler = True
-    
-    def __init__(self, label, eventName, buttonorder = 1):
-        super().__init__()
-        global x, y
-        self.eventName = eventName
-        self.label = label
-        self.buttonorder = buttonorder
-        self.bgImage = Sprite("menuButton.png") # Replace with proper resource pack image
-        self.lbl = Label(self.label, anchor_x="center", anchor_y="center")
-        
-        self.x = x / 2
-        self.TOPBTNPOS = 0.68
-        self.y = y * (self.TOPBTNPOS - (0.16 * (buttonorder - 1)))
-                
-        self.width = self.bgImage.width
-        self.height = self.bgImage.height
-        
-        self.add(self.bgImage, z = 0)
-        self.add(self.lbl, z = 1)
-        
-        self.width_range = [int(self.x - (self.bgImage.width / 2)), int(self.x + (self.bgImage.width / 2))]
-        self.height_range = [int(self.y - (self.bgImage.height / 2)), int(self.y + (self.bgImage.height / 2))]
-        
-        self.animate()
-
-        self.schedule_interval(self.setWH, 1)
-        self.resume_scheduler()
-
-    def setWH(self, dt):
-        x, y = director.window.width, director.window.height
-        scalex = x / reswidth
-        scaley = y / resheight
-        self.width_range = [int((x / 2) - ((self.bgImage.width * scalex) / 2)), int((x / 2) + ((self.bgImage.width * scalex) / 2))]
-        self.height_range = [int((y * (self.TOPBTNPOS - (0.16 * (self.buttonorder - 1)))) - ((self.bgImage.height * scaley) / 2)), int((y * (self.TOPBTNPOS - (0.16 * (self.buttonorder - 1)))) + ((self.bgImage.height * scaley) / 2))]
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        if x in range(self.width_range[0], self.width_range[1]) and y in range(self.height_range[0], self.height_range[1]):
-            self.bgImage.image = pyglet.resource.image("menuButtonHovered.png")
-        else:
-            self.bgImage.image = pyglet.resource.image("menuButton.png")
-
-    def on_mouse_press(self, x, y, buttons, modifiers):
-        if x in range(self.width_range[0], self.width_range[1]) and y in range(self.height_range[0], self.height_range[1]):
-            self.eventName()
-            self.bgImage.image = pyglet.resource.image("menuButtonClicked.png")
-            self.lbl.element.color = (0, 0, 0, 255)
-
-    def animate(self):
-        mvx = self.x
-        mvy = self.y
-        # ODD
-        if not self.buttonorder % 2 == 0:
-            self.x = -self.width
-            self.do(Delay(0.7) + AccelDeccel(MoveTo((mvx, mvy), 1.5)))
-        # EVEN
-        else:
-            self.x = x + self.width
-            self.do(Delay(0.7) + AccelDeccel(MoveTo((mvx, mvy), 1.5)))
+gameTitle = elements.titleLabel
+gameTitle.element.text = "Game Title"
 
 
 '''Game scenes'''
@@ -119,13 +53,13 @@ class MainMenuScreen(scene.Scene):
     def __init__(self):
         super(MainMenuScreen, self).__init__()
         global x, y
-        self.add(titleLabel)
-        titleLabel.do(AccelDeccel(MoveTo((x / 2, y * 0.9), 1.5)))
+        self.add(gameTitle)
+        gameTitle.do(AccelDeccel(MoveTo((x / 2, y * 0.9), 1.5)))
         
-        playButton = menuItem("Play Game", events.mainmenuevents.onPlayButtonClick, 1)
-        multiplayerButton = menuItem("Multiplayer", events.mainmenuevents.onMultiplayerButtonClick, 2)
-        settingsButton = menuItem("Settings", events.mainmenuevents.onSettingsButtonClick, 3)
-        quitButton = menuItem("Quit Game", events.mainmenuevents.onQuitButtonClicked, 4)
+        playButton = elements.MainMenuButton("Play Game", events.mainmenuevents.onPlayButtonClick, 1)
+        multiplayerButton = elements.MainMenuButton("Multiplayer", events.mainmenuevents.onMultiplayerButtonClick, 2)
+        settingsButton = elements.MainMenuButton("Settings", events.mainmenuevents.onSettingsButtonClick, 3)
+        quitButton = elements.MainMenuButton("Quit Game", events.mainmenuevents.onQuitButtonClicked, 4)
 
         self.add(playButton)
         self.add(multiplayerButton)
@@ -139,9 +73,9 @@ class loadingScreen(scene.Scene):
     def __init__(self):
         super(loadingScreen, self).__init__()
         x, y = cocos.director.director.get_window_size()
-        titleLabel.position = x / 2, y * 0.7
-        self.add(titleLabel)
-        titleLabel.do(FadeIn(1))
+        gameTitle.position = x / 2, y * 0.7
+        self.add(gameTitle)
+        gameTitle.do(FadeIn(1))
 
     def on_enter(self):
         super().on_enter()
