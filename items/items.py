4b31845f17fcb8ee1b629f3ed81d4fc51cbb3e64
alihamzamohammed <alihamzamohammed2@gmail.com>
2020-09-13 20:52:29 +0100

    TODO: Item pack loading and level depedency checking, through Class declarations

--- items/items.py
+++ items/items.py
@@ -8,3 +8,4 @@ class Item(cocos.tiles.Cell):
 
 # ?: Custom code delcared here for specific items that instantiate a class, such as trampoline needs to override existing code from the game mode to implement new functionality.
 # ?: One way in which this could be done is through decorators, but this will need thorough research in order to implement properly.
+
