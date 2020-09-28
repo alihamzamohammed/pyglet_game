c85706e988abb5ff74122a1a97799c7040b9936f
alihamzamohammed <alihamzamohammed2@gmail.com>
2020-09-28 19:57:33 +0100

    Cleanup and todo/problem removal

--- items/bouncePack/bouncePack.py
+++ items/bouncePack/bouncePack.py
@@ -3,16 +3,16 @@ import renderer as rn
 DOWN = (0, -2) # Custom DOWN value taking into account double block height of player sprite
 
 def bounce_modifier(func):
-    #pass
+    
     def bounce(value, original):
-        #print("hello this works!")
         newval = value
+    
         if rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile is not None:
             if "sourcecode" in rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile.properties:
                 if rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile.properties["sourcecode"] == "bouncePack":
                     newval = value * rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile.properties["bounce_multiplier"]
+    
         newfunc = func(newval, original)
         return newfunc
+    
     return bounce
-
-    # TODO: CHANGE THIS CODE
