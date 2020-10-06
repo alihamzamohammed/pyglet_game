88e64bbc939690c3a5762027b02465f208347fab
alihamzamohammed <alihamzamohammed2@gmail.com>
2020-10-06 17:56:01 +0100

    Added scrolling

--- scrolltest.py
+++ scrolltest.py
@@ -20,9 +20,10 @@ class ParentLayer(layer.Layer):
     def __init__(self):
         super().__init__()
         scrollManager = layer.ScrollingManager()
-        scrollLayer = ScrollLayer()
+        scrollLayer = ScrollLayer(0, 0, rx * 0.8, ry * 0.8)
         scrollBar = ScrollBar()
         
+
         scrollLayer.x = 0
         scrollLayer.y = 0
 
@@ -38,8 +39,18 @@ class ParentLayer(layer.Layer):
 
 class ScrollLayer(layer.ScrollableLayer):
 
-    def __init__(self, parallax=1):
+    def __init__(self, x, y, w, h, parallax=1):
         super().__init__(parallax=parallax)
+        self.width = w
+        self.height = h
+        self.x = x
+        self.y = y
+        self.t = sprite.Sprite("smallButton.png")
+        self.t.x = self.width / 2
+        self.t.y = self.height / 2
+        
+        self.add(self.t)
+
 
 
 class ScrollBar(layer.Layer):
@@ -60,9 +71,10 @@ class ScrollBar(layer.Layer):
         self.schedule_interval(self.setWH, 1)
         self.resume_scheduler()
 
+        #self.active = False
+
     def setWH(self, dt):
         x, y = director.window.width, director.window.height
-        print(self.img.height, self.y)
         nmin = sc.scale(int((self.x) - (self.img.width / 2)), int((self.y) - (self.img.height / 2)))
         nmax = sc.scale(int((self.x) + (self.img.width / 2)), int((self.y) + (self.img.height / 2)))
         self.width_range = [int(nmin[0]), int(nmax[0])]
@@ -79,3 +91,27 @@ class ScrollBar(layer.Layer):
     def on_mouse_press(self, x, y, buttons, modifiers):
         if x in range(self.width_range[0], self.width_range[1]) and y in range(self.height_range[0], self.height_range[1]):
             self.img.image = pyglet.resource.image("scrollbarClicked.png")
+            self.active = True
+        else:
+            self.active = False
+           
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        print(self.y, dy, y)
+        if self.active:
+            if dy > 0:
+                if not self.y < (ry - (self.img.height / 2)):
+                    self.y += dy
+            elif dy < 0:
+                if not self.y > (self.img.height / 2):
+                    self.y += dy
+
+
+
+            #if :
+            #    top = True
+            #else:
+            #    top = False
+            #
+            #if self.y > (self.img.height / 2):
+            #    bottom = True
+            #    self.y += dy
