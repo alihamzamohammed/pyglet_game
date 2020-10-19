d3d2b2d202e2d4496e3475632b1ca5b16e7d9551
alihamzamohammed <alihamzamohammed2@gmail.com>
2020-10-19 15:49:39 +0100

    Testing install functinality

--- install.py
+++ install.py
@@ -15,9 +15,6 @@ import message
 installTypes = {"mode": modes.GameMode, "itempack": items.ItemPack, "level": levels.Level}
 
 def installer(installType, dialog = "folder"):
-    #if installType == modes.GameMode or installType == items.ItemPack or installType == levels.Level:
-        
-        #if installType == modes.GameMode:\
     if installType == modes.GameMode:
         typeDict = modes.gamemodes
         typeModule = modes
@@ -38,79 +35,31 @@ def installer(installType, dialog = "folder"):
         return False
     if installPath == "" or installPath == None:
         return False
+    
     installPathList = installPath.split("\\")
-    print(installPathList)
+    
     if installPathList[-1] in list(typeDict.keys()):
-        return AlreadyInstalled(installType)
+        message.showMessage(installPathList[-1] + " is already installed!")
+        logger.addLog(installPathList[-1] + " is already installed!", logger.loglevel["info"])
+        return False
     else:
         message.showMessage("Installing " + installType.modeType + installPathList[-1])
-        #try:
-        if os.path.isdir(installPath):
-            shutil.copytree(installPath, os.getcwd() + typeModule.folder + installPathList[-1])
-        else:
-            if installPath.lower().endswith(".zip"):
-                with zipfile.ZipFile(installPath, "r") as zip_ref:
-                    zip_ref.extractall(os.getcwd() + typeModule.folder + installPathList[-1][:-4])
+        try:
+            if os.path.isdir(installPath):
+                shutil.copytree(installPath, os.getcwd() + typeModule.folder + installPathList[-1])
             else:
-                message.showMessage("The chosen extension is not a folder or zip file.")
-                logger.addLog(installPathList[-1] + " is not a valid folder or .zip file, cannot be installed!", logger.loglevel["warning"])
-                return False
-        message.showMessage("Installed " + installType.modeType + installPathList[-1])
-        logger.addLog(installType.modeType + installPathList[-1] + " installed!", logger.addLog["debug"])
-        return True
-        #except Exception as e:
-        #    message.showMessage("An error occured installing " + installType.modeType  + " " + installPathList[-1])
-        #    logger.addLog("An error occured installing " + installType.modeType + " " + installPathList[-1], logger.loglevel["warning"])
-        #    logger.addLog("Error installing " + installType.modeType + " " + installPathList[-1] + "\nException:\n" + str(e), logger.loglevel["debug"])
-        #    return False
-    
-    #     if installType == items.ItemPack:
-    #         installPath.split("\\")[-1]
-    #         if installPath.split("\\")[-1] in list(items.itempacks.keys()):
-    #             return AlreadyInstalled(installType)
-    #         else:
-    #             message.showMessage("Installing Item Pack " + installPath.split("\\")[-1])
-    #             try:
-    #                 shutil.copytree(installPath, os.getcwd() + items.folder + installPath.split("\\")[-1])
-    #                 message.showMessage("Installed Item Pack ")# + installPath.split("\\")[-1])
-    #                 logger.addLog("Item Pack " + installPath.split("\\")[-1] + " installed!", logger.addLog["debug"])
-    #                 return True
-    #             except Exception as e:
-    #                 message.showMessage("An error occured installing Item Pack " + installPath.split("\\")[-1])
-    #                 logger.addLog("An error occured installing Item Pack " + installPath.split("\\")[-1], logger.loglevel["warning"])
-    #                 logger.addLog("Error installing item pack " + installPath.split("\\")[-1] + "\nException:\n" + str(e), logger.loglevel["debug"])
-    #                 return False
-        
-    #     if installType == levels.Level:
-    #         level = installPath.split("\\")[-1]
-    #         if level in list(levels.levels.keys()):
-    #             return AlreadyInstalled(installType)
-    #         else:
-    #             message.showMessage("Installing Level " + level)
-    #             print("installing")
-    #             try:
-    #                 shutil.copytree(installPath, os.getcwd() + levels.folder + level)
-    #                 print("copied")
-    #                 message.showMessage("Installed Level ")# + level)
-    #                 logger.addLog("Level " + level + " installed!", logger.addLog["debug"])
-    #                 return True
-    #             except Exception as e:
-    #                 message.showMessage("An error occured installing Level " + level)
-    #                 logger.addLog("An error occured installing Level " + level, logger.loglevel["warning"])
-    #                 logger.addLog("Error installing level " + level + "\nException:\n" + str(e), logger.loglevel["debug"])
-    #                 return False
-    # else:
-    #     return InvalidType()
-
-
-class AlreadyInstalled():
-
-    def __init__(self, installType):
-        super().__init__()
-        self.alreadyInstalledType = installType
-
-class InvalidType():
-    pass
-
-def install(installType):
-    pass
+                if installPath.lower().endswith(".zip"):
+                    with zipfile.ZipFile(installPath, "r") as zip_ref:
+                        zip_ref.extractall(os.getcwd() + typeModule.folder + installPathList[-1][:-4])
+                else:
+                    message.showMessage("The chosen extension is not a folder or zip file.")
+                    logger.addLog(installPathList[-1] + " is not a valid folder or .zip file, cannot be installed!", logger.loglevel["warning"])
+                    return False
+            message.showMessage("Installed " + installType.modeType  + " " + installPathList[-1])
+            logger.addLog(installType.modeType + " " + installPathList[-1] + " installed!", logger.loglevel["debug"])
+            return True
+        except Exception as e:
+            message.showMessage("An error occured installing " + installType.modeType  + " " + installPathList[-1])
+            logger.addLog("An error occured installing " + installType.modeType + " " + installPathList[-1], logger.loglevel["warning"])
+            logger.addLog("Error installing " + installType.modeType + " " + installPathList[-1] + "\nException:\n" + str(e), logger.loglevel["debug"])
+            return False
---
+++ levels/v4w-patcher-v1.1-release (1)/v4w-patcher-v1.1-release/README.md
@@ -0,0 +1,29 @@
+# ViPER4Windows-Patcher
+A patcher script for fix ViPER4Windows issues on Windows-10.
+
+### Screenshot:
+![Alt text](/../screenshots/v4w-patcher_pic1.png?raw=true)
+
+### Features:
+* Fixes incompatibility issues.
+* Fixes registry entry issues.
+* Handy portable script, run from anywhere.
+* Integrated administrative privileges request.
+
+### Applicable OS:
+* Windows-10+
+
+### Download:
+#### [ViPER4Windows-Patcher (Latest)](https://github.com/metaspook/ViPER4Windows-Patcher/releases/download/v1.1/v4w-patcher-v1.1-release.zip)
+
+### Instructions:
+* Make sure "ViPER4Windows" installed in your PC.
+* Download "v4w-patcher-vx.x-release.zip"
+* Right click on "v4w-patcher-vx.x-release.zip" select 'Extract All'.
+* Inside extracted folder run "v4w-patcher.bat"
+* You can move and run this script from anywhere. 😉
+
+### Change Log:
+* v1.1: Improved Registry fix, admin request and UI.
+* ----- Added 'Launch Configurator'
+* v1.0: Initial Release.
