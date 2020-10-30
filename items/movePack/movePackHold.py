ed8446247e86b360cc5a0deb6a4af8ab5c12c28e
alihamzamohammed <alihamzamohammed2@gmail.com>
2020-10-30 16:53:14 +0000

    Separate source code for hold block in move pack

--- items/movePack/movePackHold.py
+++ items/movePack/movePackHold.py
@@ -9,26 +9,30 @@ onTile = False
 currentTime = 0
 duration = 0
 multiplier = 0
+immuneTime = 0
 def move_modifier(func):
 
     def move(value):
-        global onTile, currentTime, duration, multiplier
+        global onTile, currentTime, duration, multiplier, immuneTime
         newval = value
-        if not onTile:
+
+        if not onTile and t.perf_counter() > immuneTime + 1:
             if rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile is not None:
                 if "sourcecode" in rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile.properties:
                     if rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile.properties["sourcecode"] == "movePack":
-                        if rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile.id != "hold":
+                        if rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile.id == "hold":
                             multiplier = rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile.properties["move_multiplier"]
                             duration = rn.tilemap_walls.get_neighbor(rn.tilemap_walls.get_at_pixel(rn.player.x, rn.player.y), DOWN).tile.properties["move_duration"]
                             onTile = True
                             currentTime = t.perf_counter()
-        else:
+        elif onTile:
             if t.perf_counter() < currentTime + duration:
                 newval = newval * multiplier
             else:
                 newval = value
                 onTile = False
+                immuneTime = t.perf_counter()
+                print("immune 1 sec")
         
         newfunc = func(newval)
         return newfunc
