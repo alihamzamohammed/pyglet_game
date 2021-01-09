9237bbc9a1951cb82b35751d6a98532d89342112
alihamzamohammed <alihamzamohammed2@gmail.com>
2021-01-09 21:16:57 +0000

    Changed save filepath to match

--- levels/__init__.py
+++ levels/__init__.py
@@ -103,7 +103,7 @@ class Level():
                     element.set("type", required)
                     element.text = item
                     self._lvl.getroot().append(element)
-        self._lvl.write(self.folder)
+        self._lvl.write(self.folder + "\\" + self.idx + ".xml")
 
 class LevelCorrupt(Exception):
 
