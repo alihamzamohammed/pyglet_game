7810210cada05e2334fcf0b91d4b9c0856898e5f
alihamzamohammed <alihamzamohammed2@gmail.com>
2021-01-09 16:14:00 +0000

    Corrent attributes for discard button

--- leveleditor.py
+++ leveleditor.py
@@ -372,8 +372,8 @@ class LevelEditor(scene.Scene):
         self.saveButton.y =  int(self.headerLayer.height / 2)
 
         self.discardButton = elements.mediumButton("DISCARD", events.mainmenuevents.onPlayButtonClick)
-        self.discardButton.lbl.element.font_size = 14
-        self.discardButton.x = int(self.headerLayer.width * 0.927)
+        self.discardButton.lbl.element.font_size = 13
+        self.discardButton.x = int(self.headerLayer.width * 0.857)
         self.discardButton.y = int(self.headerLayer.height / 2)
         
         #self.editButton
